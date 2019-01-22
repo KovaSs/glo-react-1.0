071783708625dd9dfb416f86c97addd8f99568b0
Denis Isaev <isaev_di@ursip.ru>
2019-01-22 15:39:00 +0300

    add information no data filds

--- got/src/components/RandomChar/index.js
+++ got/src/components/RandomChar/index.js
@@ -54,23 +54,23 @@ export default class RandomChar extends Component {
 					error ?	<ErrorMessage/> : <Spinner/>
 					:
 					<div>
-						<h4>Random Character: {name}</h4>
+						<h4>Random Character: {name || "No data:("}</h4>
 						<ul className="list-group list-group-flush">
 							<li className="list-group-item d-flex justify-content-between">
 								<span className="term">Gender </span>
-								<span>{gender}</span>
+								<span>{gender || "No data:("}</span>
 							</li>
 							<li className="list-group-item d-flex justify-content-between">
 								<span className="term">Born </span>
-								<span>{born}</span>
+								<span>{born || "No data:("}</span>
 							</li>
 							<li className="list-group-item d-flex justify-content-between">
 								<span className="term">Died </span>
-								<span>{died}</span>
+								<span>{died || "No data:("}</span>
 							</li>
 							<li className="list-group-item d-flex justify-content-between">
 								<span className="term">Culture </span>
-								<span>{culture}</span>
+								<span>{culture || "No data:("}</span>
 							</li>
 						</ul>
 					</div>
