413d43c9e1ec109e8065274fd195377f619f83a4
Isaev Denis <sr_relikt@mail.ru>
2019-02-03 15:59:17 +0300

    using action creator

--- redux-app/src/index.js
+++ redux-app/src/index.js
@@ -6,22 +6,34 @@ const reducer = (state = 0, action) => {
       return state + 1;
     case 'DEC' :
       return state - 1;
+    case 'RND' :
+      return state + action.value;
     default :
       return state
   }
 }
 
+const inc = () => ({type: 'INC'});
+const dec = () => ({type: 'DEC'});
+const rnd = value => ({type: 'RND', value});
+
 const store = createStore(reducer);
-store.subscribe(() => {
-  console.log(`getState ->`,store.getState())  
-})
 
 document.getElementById('inc').addEventListener('click',() => {
-  store.dispatch({type: 'INC'})
+  store.dispatch(inc())
 })
 document.getElementById('dec').addEventListener('click',() => {
-  store.dispatch({type: 'DEC'})
+  store.dispatch(dec())
 })
+document.getElementById('rnd').addEventListener('click',() => {
+  const value = Math.floor(Math.random() * 10);
+  store.dispatch(rnd(value))
+})
+
+const update = () => {
+  document.getElementById('counter').textContent = store.getState()
+}
+store.subscribe(update)
 
 // store.dispatch({type: 'INC'})
 // store.dispatch({type: 'INC'})
