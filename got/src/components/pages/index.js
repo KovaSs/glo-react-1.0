1d301b932922922afbad47219dc31430fabccd5d
Isaev Denis <sr_relikt@mail.ru>
2019-02-03 15:40:49 +0300

    remove got aplication

--- got/src/components/pages/index.js
+++
@@ -1,7 +0,0 @@
-import CharacterPage from './CharactePage'
-import BooksPage from './BooksPage'
-import HousesPage from './HousesPage'
-import BooksItem from './BooksItem'
-import MainPage from './MainPage'
-
-export {CharacterPage, BooksPage, HousesPage, BooksItem, MainPage}
