9c39a26af02cce7e2c25e4999fe61914343fb00e
Isaev Denis <sr_relikt@mail.ru>
2019-02-04 22:00:48 +0300

    add begin react + redux app

---
+++ redux-app/src/Counter.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import {connect} from 'react-redux';
+import * as actions from '../actions';
+
+const Counter = ({counter, inc, dec, res}) => {
+	return (
+		<div className ="background">
+			<div className ="counter-block">
+				<div className ="counter">{counter}</div>
+			</div>
+			<button onClick = {inc} className ="plus-block"></button>
+			<button onClick = {dec}  className ="minus-block"></button>
+			<button onClick = {res} className ="reset-block"></button>
+	</div>
+	);
+}
+
+const mapStateToProps = (state) => {
+	return {
+		counter: state
+	}
+}
+
+export default connect(mapStateToProps, actions)(Counter); 
--- redux-app/src/index.js
+++ redux-app/src/index.js
@@ -1,35 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
 import {createStore} from 'redux';
+import {Provider} from 'react-redux';
+import reducer from './reducer';
+import App from './App';
 
-const reducer = (state = 0, action) => {
-	switch (action.type) {
-		case 'INC':
-			return state + 1;
-		case 'DEC':
-			return state - 1;
-		case 'RND':
-			return state + action.value;
-		case 'RES':
-			return 0
-		default:
-			return state;
-	}
-}
 const store = createStore(reducer);
 
-const inc = () => ({type: 'INC'});
-const dec = () => ({type: 'DEC'});
-const res = () => ({type: 'RES'});
-
-document.getElementById('inc').addEventListener('click', () => {
-	store.dispatch(inc());
-});
-document.getElementById('dec').addEventListener('click', () => {
-	store.dispatch(dec());
-});
-document.getElementById('reset').addEventListener('click', () => {
-	store.dispatch(res());
-});
-const update = () => {
-	document.getElementById('counter').textContent = store.getState();
-}
-store.subscribe(update);
+ReactDOM.render(
+	<Provider store = {store}>
+		<App/>
+	</Provider>
+	, document.getElementById('root'));
