2a8ce9694c3393a15f1289118e96e0cc12dc3cce
Isaev Denis <sr_relikt@mail.ru>
2019-02-06 23:49:33 +0300

    add testing functions

--- got/src/components/RandomChar/index.js
+++ got/src/components/RandomChar/index.js
@@ -46,26 +46,26 @@ class RandomChar extends Component {
 	}
 
 	render() {
-		const {char: {name, gender, born, died, culture}, loading, error} = this.state
+		const {char: {name, gender, born, died, culture}, loading, error} = this.state;
 		const randomChar = (
 			<div>
-				<h4>Random Character: {name || "¯\\_(ツ)_/¯"}</h4>
+				<h4>Random Character: {name}</h4>
 				<ul className="list-group list-group-flush">
 					<li className="list-group-item d-flex justify-content-between">
 						<span className="term">Gender </span>
-						<span>{gender || "¯\\_(ツ)_/¯"}</span>
+						<span>{gender}</span>
 					</li>
 					<li className="list-group-item d-flex justify-content-between">
 						<span className="term">Born </span>
-						<span>{born || "¯\\_(ツ)_/¯"}</span>
+						<span>{born}</span>
 					</li>
 					<li className="list-group-item d-flex justify-content-between">
 						<span className="term">Died </span>
-						<span>{died || "¯\\_(ツ)_/¯"}</span>
+						<span>{died}</span>
 					</li>
 					<li className="list-group-item d-flex justify-content-between">
 						<span className="term">Culture </span>
-						<span>{culture || "¯\\_(ツ)_/¯"}</span>
+						<span>{culture}</span>
 					</li>
 				</ul>
 			</div>
--- got/src/components/RandomChar/randomChar.test.js
+++ got/src/components/RandomChar/randomChar.test.js
@@ -1,10 +1,44 @@
 import React from 'react';
-import * as renderer from 'react-test-renderer';
 import RandomChar from './index';
+import {shallow} from 'enzyme';
 
 describe('Testing <RandomChar/>', () => {
-  it('RandomChar have rendered correctly', () => {
-    const char = renderer.create(<RandomChar/>).toJSON();
-    expect(char).toMatchSnapshot();
+  const char = shallow(<RandomChar/>);
+
+  describe('Testing snap & state', () => {
+    it('RandomChar have rendered correctly', () => {
+      expect(char).toMatchSnapshot();
+    });
+
+    it('RandomChar state "char" is empty object', () => {
+      expect(char.state().char).toBeObject();
+    });
+
+    it('RandomChar state "loading" is true', () => {
+      expect(char.state().loading).toBeTruthy();
+    });
+
+    it('RandomChar state "error" is false', () => {
+      expect(char.state().error).toBeFalsy();
+    });
+  });
+
+  describe('Heandlears tests', () => {
+    it('Testing onCharLoaded', () => {
+      char.instance().onCharLoaded();
+      expect(char.state().loading).toBeFalsy();
+    });
+
+    it('Testing onError', () => {
+      char.instance().onError();
+      expect(char.state().loading).toBeFalsy();
+      expect(char.state().error).toBeTruthy();
+    });
+
+    it('Testing updateChar', () => {
+      char.instance().updateChar();
+      expect(char.state().loading).toBeFalsy();
+    });
   })
+
 })
